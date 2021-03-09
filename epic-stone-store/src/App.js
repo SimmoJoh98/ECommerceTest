673545507a9349fe5eb8af48a2b5e40ee1fb4cd5
Xeilon <simmojoh98@gmail.com>
2021-03-09 11:51:14 -0700

    Sign In / Sign up forms.

--- epic-stone-store/src/App.js
+++ epic-stone-store/src/App.js
@@ -4,6 +4,8 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component.jsx'
+import SignInUp from './pages/sign-in-and-sign-up/sign-in-up.component.jsx'
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInUp}/>
       </Switch>
     </div>
   );
---
+++ epic-stone-store/src/pages/sign-in-and-sign-up/sign-in-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component.jsx'
+
+
+const SignInUp = () => (
+    <div className='sign-in-up'>
+        <SignIn />
+    </div>
+)
+
+export default SignInUp;
